a8a11fd79180deb82a762312eecf2d474ed02af6
RedHotChiliPeppers35 <ata.berk.cinetci@icloud.com>
2022-12-10 04:01:32 +0300

    initial xCode project creation

---
+++ youtubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeApp
+//
+//  Created by Ata Berk Cinetci on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
